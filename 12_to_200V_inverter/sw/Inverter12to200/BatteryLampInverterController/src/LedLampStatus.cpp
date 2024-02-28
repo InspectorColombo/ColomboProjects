d0e4f28a118bfd2d8a92e682dc4bd191a2e1fc80
InspectorColombo <einst2004@rambler.ru>
2024-02-28 22:36:51 +0300

    12 to 200V converter. Do some clean up.

--- 12_to_200V_inverter/sw/Inverter12to200/BatteryLampInverterController/src/LedLampStatus.cpp
+++
@@ -1,25 +0,0 @@
-/*
- * LedLampStatus.cpp
- *
- * Created: 27.01.2024 23:58:39
- *  Author: Goggy
- */ 
-
-#include "LedLampStatus.hpp"
-
-namespace LedLamp
-{
-// static const uint16_t ST_OVERTEMPERATURE			= 0b00000001;
-// static const uint16_t ST_CONVERTER_ON				= 0b00000010;
-// static const uint16_t ST_LOW_BATTERY				= 0b00000100;
-// static const uint16_t ST_LOW_BATTERY_WARNING		= 0b00001000;
-// static const uint16_t ST_IGNORE_LOW_BATTERY			= 0b00010000;
-// static const uint16_t ST_PREV_IGNORE_LOW_BATTERY	= 0b00100000;
-// static const uint16_t ST_KEY_ON						= 0b01000000;// static const uint16_t ST_PREV_KEY_ON				= 0b10000000;// static const uint16_t ST_ADD_VOLTAGE				= 0b0000000100000000;// static const uint16_t ST_CHARGE_FINISH				= 0b000000100000000
-
-
-
-
-
-
-}	// namespace LedLamp
