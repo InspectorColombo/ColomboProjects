a1b19b2d416a720468b787d5e4a5495b75da02d3
InspectorColombo <einst2004@rambler.ru>
2024-05-01 09:16:24 +0300

    12 to 200V inverter. New board with ETD transformers. Route.

---
+++ 1.bat
@@ -0,0 +1,2 @@
+git add C:\__workspace\ColomboProjects\12_to_200V_inverter\schematic\12to220inverterVer3.brd
+git add C:\__workspace\ColomboProjects\12_to_200V_inverter\schematic\12to220inverterVer3.sch
