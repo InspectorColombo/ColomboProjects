b1ddb6448e3f3e37e151cbfbf6f2036d4352285c
InspectorColombo <einst2004@rambler.ru>
2024-11-16 01:32:32 +0300

    test.

--- 1.bat
+++
@@ -1,2 +0,0 @@
-git add C:\__workspace\ColomboProjects\12_to_200V_inverter\schematic\12to220inverterVer3.brd
-git add C:\__workspace\ColomboProjects\12_to_200V_inverter\schematic\12to220inverterVer3.sch
