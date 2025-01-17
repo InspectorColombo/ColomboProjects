92e522e649fa1436acb61a0ef837ccda29165956
InspectorColombo <einst2004@rambler.ru>
2025-01-17 07:12:16 +0300

    Humidity metter. Move BME280 to new fast SW I2C.

--- HumidityMetter/sw_ver2/HumidityMetter/Release/Src/BME280Sensor.d
+++ HumidityMetter/sw_ver2/HumidityMetter/Release/Src/BME280Sensor.d
@@ -1,17 +1,17 @@
-Src/BME280Sensor.o: ../Src/BME280Sensor.cpp ../Inc/BME280Sensor.hpp \
+Src/BME280Sensor.o: ../Src/BME280Sensor.cpp \
  C:/__workspace/ColomboProjects/HumidityMetter/sw_ver2/HumidityMetter/CMSIS/inc/stm32f103xb.h \
  C:/__workspace/ColomboProjects/HumidityMetter/sw_ver2/HumidityMetter/CMSIS/inc/core_cm3.h \
  C:/__workspace/ColomboProjects/HumidityMetter/sw_ver2/HumidityMetter/CMSIS/inc/cmsis_version.h \
  C:/__workspace/ColomboProjects/HumidityMetter/sw_ver2/HumidityMetter/CMSIS/inc/cmsis_compiler.h \
  C:/__workspace/ColomboProjects/HumidityMetter/sw_ver2/HumidityMetter/CMSIS/inc/cmsis_gcc.h \
  C:/__workspace/ColomboProjects/HumidityMetter/sw_ver2/HumidityMetter/CMSIS/inc/system_stm32f1xx.h \
- ../Inc/DelayTimer.hpp ../Inc/I2cRxTx.hpp
-../Inc/BME280Sensor.hpp:
+ ../Inc/BME280Sensor.hpp ../Inc/I2cSw.hpp ../Inc/DelayTimer.hpp
 C:/__workspace/ColomboProjects/HumidityMetter/sw_ver2/HumidityMetter/CMSIS/inc/stm32f103xb.h:
 C:/__workspace/ColomboProjects/HumidityMetter/sw_ver2/HumidityMetter/CMSIS/inc/core_cm3.h:
 C:/__workspace/ColomboProjects/HumidityMetter/sw_ver2/HumidityMetter/CMSIS/inc/cmsis_version.h:
 C:/__workspace/ColomboProjects/HumidityMetter/sw_ver2/HumidityMetter/CMSIS/inc/cmsis_compiler.h:
 C:/__workspace/ColomboProjects/HumidityMetter/sw_ver2/HumidityMetter/CMSIS/inc/cmsis_gcc.h:
 C:/__workspace/ColomboProjects/HumidityMetter/sw_ver2/HumidityMetter/CMSIS/inc/system_stm32f1xx.h:
+../Inc/BME280Sensor.hpp:
+../Inc/I2cSw.hpp:
 ../Inc/DelayTimer.hpp:
-../Inc/I2cRxTx.hpp:
--- HumidityMetter/sw_ver2/HumidityMetter/Release/Src/main.d
+++ HumidityMetter/sw_ver2/HumidityMetter/Release/Src/main.d
@@ -8,9 +8,9 @@ Src/main.o: ../Src/main.cpp \
  ../Inc/LcdScreen.hpp ../Inc/DelayTimer.hpp ../Inc/PowerControl.hpp \
  ../Inc/GpioConfigurator.hpp ../Inc/ErrorLed.hpp ../Inc/AM2320Sensor.hpp \
  ../Inc/DelayTimer.hpp ../Inc/ErrorLed.hpp ../Inc/I2cRxTx.hpp \
- ../Inc/BME280Sensor.hpp ../Inc/KeyPressDetector.hpp ../Inc/I2cRxTx.hpp \
- ../Inc/BatterySensor.hpp ../Inc/AdcSingleRead.hpp ../Inc/SSD1306.hpp \
- ../Inc/I2cSw.hpp
+ ../Inc/BME280Sensor.hpp ../Inc/I2cSw.hpp ../Inc/KeyPressDetector.hpp \
+ ../Inc/I2cRxTx.hpp ../Inc/BatterySensor.hpp ../Inc/AdcSingleRead.hpp \
+ ../Inc/SSD1306.hpp
 C:/__workspace/ColomboProjects/HumidityMetter/sw_ver2/HumidityMetter/CMSIS/inc/stm32f103xb.h:
 C:/__workspace/ColomboProjects/HumidityMetter/sw_ver2/HumidityMetter/CMSIS/inc/core_cm3.h:
 C:/__workspace/ColomboProjects/HumidityMetter/sw_ver2/HumidityMetter/CMSIS/inc/cmsis_version.h:
@@ -27,9 +27,9 @@ C:/__workspace/ColomboProjects/HumidityMetter/sw_ver2/HumidityMetter/CMSIS/inc/s
 ../Inc/ErrorLed.hpp:
 ../Inc/I2cRxTx.hpp:
 ../Inc/BME280Sensor.hpp:
+../Inc/I2cSw.hpp:
 ../Inc/KeyPressDetector.hpp:
 ../Inc/I2cRxTx.hpp:
 ../Inc/BatterySensor.hpp:
 ../Inc/AdcSingleRead.hpp:
 ../Inc/SSD1306.hpp:
-../Inc/I2cSw.hpp:
